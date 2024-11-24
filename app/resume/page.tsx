7d4a18f56bd8914093d6315b75fa03e0db48edca
David Germiquet <david@trulycanadian.net>
2024-11-24 12:33:04 -0500

    Better formatting

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -9,7 +9,7 @@ export default  function resume() {
     return (
       <div className="div_main">
         <main className="main_class">
-            <div className="flex-none lg:flex lg:flex-row mr-40 ml-5  mt-10">
+            <div className="flex-none lg:flex lg:flex-row mr-20 md:mr-40 ml-5  mt-10">
               <div className="flex-none lg:flex lg:flex-col text-left pl-5 pr-7 mb-10"> 
               <div className="">
                <Contact/>
